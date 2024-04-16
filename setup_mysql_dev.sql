d39635aa9d0d7a0da964653010bc4acd04cda484
Benjamin Torfu <btorfu@proton.me>
2024-04-16 13:43:28 +0000

    sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- Create a database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create a new user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+USE hbnb_dev_db;
+
+-- Grant all priveledge to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbtn_dev'@'localhost';
+
+-- Grant select privileges to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbtn_dev'@'localhost';
