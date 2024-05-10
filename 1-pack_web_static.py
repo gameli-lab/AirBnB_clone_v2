35d4f6e836f8f4d0d84b52b621c34e4d83bfb374
Benjamin Torfu <btorfu@proton.me>
2024-05-10 11:46:06 +0000

    web

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,28 +11,22 @@ from datetime import datetime
 def do_pack(c):
     """
     Compresses the 'web_static' directory into a tar archive and saves it in the 'versions' directory.
+
     """
-    """ Create the 'versions' directory if it doesn't exist """
     if not os.path.exists('versions'):
         os.makedirs('versions')
 
-    """ Create a timestamp in the format: YYYYMMDDHHMMSS """
     now = datetime.now()
     timestamp = now.strftime("%Y%m%d%H%M%S")
 
-    """ Create the archive name """
     arch_name = f"web_static_{timestamp}.tgz"
 
-    """ Create the full path of the archive """
     arch_path = os.path.join("versions", arch_name)
 
-    """ Change to the 'web_static' directory and execute the tar command """
     with c.cd('web_static'):
         res = c.run(f"tar -cvzf {arch_path} .")
 
-    """ If the tar command fails, return None """
     if res.failed:
         return (None)
 
-    """ Return the path of the created archive """
     return (arch_path)
