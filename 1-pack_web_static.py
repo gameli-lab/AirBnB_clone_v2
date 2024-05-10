855039b75f20e3791b0efa0dc77ca6657c3c1675
Benjamin Torfu <btorfu@proton.me>
2024-05-10 11:35:02 +0000

    nginx

---
+++ 1-pack_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Using fabric to compress files
+"""
+from fabric import task
+import os
+from datetime import datetime
+
+
+def do_pack(c):
+    """
+    Compresses the 'web_static' directory into a tar archive and saves it in the 'versions' directory.
+    
+    Parameters:
+        c (Connection): The connection object used to execute the command.
+    
+    Returns:
+        str or None: The path of the created archive if successful, None otherwise.
+    """
+    if not os.path.exists('versions'):
+        os.makedirs('versions')
+
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+    arch_name = f"web_static_{timestamp}.tgz"
+    arch_path = os.path.join("versions", arch_name)
+
+    with c.cd('web_static'):
+        res = c.run(f"tar -cvzf {arch_path} .")
+        if res.failed:
+            return (None)
+
+    return (arch_path)
