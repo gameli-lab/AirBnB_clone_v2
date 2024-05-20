fee74c233928735a669515e1583f35a0e0a5a2e8
Benjamin Torfu <btorfu@proton.me>
2024-05-20 14:53:51 +0000

    flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -32,6 +32,7 @@ def python(text="is_cool"):
     return f'Python {escape(space)}'
 
 
+@app.route('/number/', strict_slashes=False)
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return f'{n}'
